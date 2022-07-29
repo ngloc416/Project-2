828a924a1a13de10f89e1a75413bfe375f066a86
ngloc416 <nguyendinhloc1102001@gmail.com>
2022-07-29 19:39:03 +0700

    de thi + dap an + tu vung

--- routes/index.js
+++ routes/index.js
@@ -1,41 +1,60 @@
-var express = require("express");
+var express = require('express');
 var router = express.Router();
 
-var fs = require("fs");
-var lessons = fs.readdirSync("./public/data/lessons");
-var documents = fs.readdirSync("./public/data/documents");
+var fs = require('fs');
+var lessons = fs.readdirSync('./public/data/lessons');
+var documents = fs.readdirSync('./public/data/documents');
+var vocabulary = fs.readdirSync('./public/data/vocabulary');
+var exams = fs.readdirSync('./public/data/exams');
+var keys = fs.readdirSync('./public/data/keys');
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("index", { title: req.path });
+router.get('/', function (req, res, next) {
+  res.render('index', { title: req.path });
 });
 
-router.get("/dongtubatquytac", function (req, res) {
-  res.render("index", { title: req.path });
+router.get('/dongtubatquytac', function (req, res) {
+  res.render('index', { title: req.path });
 });
 
-router.get("/nguphap", function (req, res) {
-  res.render("index", { title: req.path, lessons: lessons });
+router.get('/nguphap', function (req, res) {
+  res.render('index', { title: req.path, lessons: lessons });
 });
 
-router.get("/nguphap/:lesson", function (req, res) {
-  res.render("index", { title: req.params.lesson });
+router.get('/nguphap/:lesson', function (req, res) {
+  res.render('index', { title: req.params.lesson });
 });
 
-router.get("/tuvung", function (req, res) {
-  res.render("index", { title: req.path });
+router.get('/tuvung', function (req, res) {
+  res.render('index', { title: req.path, vocabulary: vocabulary });
 });
 
-router.get("/tailieu", function (req, res) {
-  res.render("index", { title: req.path, documents: documents });
+router.get('/tuvung/:vocabulary', function (req, res) {
+  res.render('index', { title: req.params.vocabulary });
 });
 
-router.get("/tailieu/:document", function (req, res) {
-  res.render("index", { title: req.params.document });
+router.get('/tailieu', function (req, res) {
+  res.render('index', { title: req.path, documents: documents });
 });
 
-router.get("/dethi", function (req, res) {
-  res.render("index", { title: req.path });
+router.get('/tailieu/:document', function (req, res) {
+  res.render('index', { title: req.params.document });
+});
+
+router.get('/dethi', function (req, res) {
+  res.render('index', { title: req.path, exams: exams });
+});
+
+router.get('/dethi/:exam', function (req, res) {
+  res.render('index', { title: req.params.exam });
+});
+
+router.get('/dapan', function (req, res) {
+  res.render('index', { title: req.path, keys: keys });
+});
+
+router.get('/dapan/:key', function (req, res) {
+  res.render('index', { title: req.params.key });
 });
 
 module.exports = router;
